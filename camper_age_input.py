515bc1a512523b5059421207da838f61f92d2855
rachelli12 <65979017+rachelli12@users.noreply.github.com>
2020-06-02 18:57:37 -0500

    Add files via upload

--- camper_age_input.py
+++ camper_age_input.py
@@ -1,5 +1,15 @@
+"""
+Program Name: camper_age_input.py
+Author: Rachel Li
+Last date modified: 06/02/2020
+
+The purpose of the program is to convert age in months to years.
+Convert age in months to years via function call.
+The input is an integer representing the age in years.
+The output is an integer representing the age in months.
+"""
 def convert_to_months(x):
     pass
 
-if __name__=='__main__':
+if __name__ == '__main__':
     pass
--- test_functions.py
+++ test_functions.py
@@ -1,10 +1,18 @@
+"""
+Program name: test_functions.py
+Author: Rachel Li
+Last date modified: 06/02/2020
+
+Purpose of this program is to test function.
+"""
 import unittest
-from main import camper_age_input
+from main import camper_age_input as cai
 
 
 class FunctionTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(False, True)
+    def test_4_years_to_months(self):
+        #4 years old should be 48 months
+        self.assertEqual(48, cai.convert_to_months(4))
 
 if __name__=='__main__':
     unittest.main()
