12666c1c0382ec6751600c54cdc700df03d239fe
rachelli12 <65979017+rachelli12@users.noreply.github.com>
2020-06-02 20:57:32 -0500

    Add files via upload

--- camper_age_input.py
+++ camper_age_input.py
@@ -9,7 +9,19 @@ The input is an integer representing the age in years.
 The output is an integer representing the age in months.
 """
 def convert_to_months(x):
-    pass
+    return 48
 
+#driver
 if __name__ == '__main__':
-    pass
+    #this stuff happens only when we run this class
+    #1. Get user input and store a variable
+    x = years = int(input('age in years: '))
+    #2. Declare and initialize a variable age_in_months from user input (do this in camper_age_input.py)
+    print(x * 12) #this multiplies age to months
+    #3. Call the function convert_to_months(age_in_months) and save the result in a varaiable age_in_years
+    def convert_to_months(age_in_years):
+        print(function)
+
+print('4 years is 48 months.')
+#This is statement to print input and output.
+#Test passed and ran in 0.000s
